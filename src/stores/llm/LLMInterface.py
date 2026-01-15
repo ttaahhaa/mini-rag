b1f9ac378e2c582435833a0340d15c992ef2e4a7
ttaahhaa <taha.almasri@ymail.com>
2026-01-15 11:47:26 +0300

    llm interface created

---
+++ src/stores/llm/LLMInterface.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class LLMInterface(ABC):
+    @abstractmethod
+    def set_generation_model(self, model_id: str):
+        pass
+
+    @abstractmethod
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        pass
+
+    @abstractmethod
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int = None, temperature: float = None) -> str:
+        pass
+
+    @abstractmethod
+    def embed_text(self, text, document_type = None) -> list[float]:
+        pass
+
+    @abstractmethod
+    def construct_prompt(self, prompt: str, role: str = "user") -> dict:
+        pass
