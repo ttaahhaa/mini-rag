9d213209e9ae858c997d2425e345587404fd1174
ttaahhaa <taha.almasri@ymail.com>
2026-01-17 19:41:03 +0300

    NLP Controller updated

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -1,6 +1,6 @@
 from .BaseController import BaseController
 from models import ResponseSignal
-
+from models.db_schemas import ProjectSchema
 class NLPController(BaseController):
     def __init__(self, generation_client, embedding_client, vectordb_client):
         super().__init__()
@@ -9,4 +9,17 @@ class NLPController(BaseController):
         self.embedding_client = embedding_client
         self.vectordb_client = vectordb_client
 
-        
+
+    def create_collection_name(self, project_id: str):
+        return f"Collection_{project_id}".strip()
+    
+    def reset_vector_db_collection(self, project: ProjectSchema):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        return self.vectordb_client.delete_collection(collection_name=collection_name)
+    
+    def get_vector_collection_info(self, collection_name, project: ProjectSchema):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
+        return collection_info
+    
+    
